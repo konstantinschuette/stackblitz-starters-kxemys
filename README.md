5c25a3e5937e0b2b3c3ac2527540d7a0a4b09408
konstantinschuette <schuettekonstantin@gmail.com>
2024-05-13 17:57:31 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# stackblitz-starters-kxemys
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/konstantinschuette/stackblitz-starters-kxemys)
---
+++ src/main.ts
@@ -0,0 +1,6 @@
+import { Component } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
+import 'zone.js';
+import { AppComponent } from './app.component';
+
+bootstrapApplication(AppComponent);
